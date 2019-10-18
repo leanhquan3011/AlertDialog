1888e2f41035589a3559c428054e724a8ce21fce
Le Anh Quan <leanhquan31198@gmail.com>
2019-10-18 20:26:43 +0700

    Alert Cosntruct

--- app/src/main/java/leanhquan/laq/alertdialog/MainActivity.java
+++ app/src/main/java/leanhquan/laq/alertdialog/MainActivity.java
@@ -1,17 +1,49 @@
 package leanhquan.laq.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnAlertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Order order = new FastFoodOderBuilder()
-                    .orderBread(BreadType.BEEF)
-                    .build();
+        mBtnAlertDialog = findViewById(R.id.AlertDialog);
+        mBtnAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Muon gi?");
+                alert.setMessage("Xac thuc");
+                alert.setIcon(R.mipmap.ic_launcher); // <150
+                alert.setCancelable(false);
+                alert.setNegativeButton("co", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                alert.setPositiveButton("Khong", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                alert.setNeutralButton("huy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                alert.show();
+            }
+        });
     }
 }
