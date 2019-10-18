fad0961f46269c8dfd34b457f0596bccf45a0884
Le Anh Quan <leanhquan31198@gmail.com>
2019-10-18 20:47:58 +0700

    alert MutipleChoice and SingleChoice

--- app/src/main/java/leanhquan/laq/alertdialog/MainActivity.java
+++ app/src/main/java/leanhquan/laq/alertdialog/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,27 +22,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
                 alert.setTitle("Muon gi?");
-                alert.setMessage("Xac thuc");
+             //   alert.setMessage("Xac thuc"); // message or checkbox
                 alert.setIcon(R.mipmap.ic_launcher); // <150
                 alert.setCancelable(false);
-                alert.setNegativeButton("co", new DialogInterface.OnClickListener() {
+                //Mutiplechoice
+//                String[] arrayAnimal = {"Cat","Dog","Pig","Dophin","Mouse","Clown"};
+//                boolean[] arrayChecked = {false,false,false,false,false,false};
+//                alert.setMultiChoiceItems(arrayAnimal, arrayChecked, new DialogInterface.OnMultiChoiceClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+//                        if(b)
+//                            Toast.makeText(MainActivity.this, "Da check", Toast.LENGTH_SHORT).show();
+//                        else
+//                            Toast.makeText(MainActivity.this, "Da check", Toast.LENGTH_SHORT).show();
+//
+//                    }
+//                });
+                String[] arrayAnimal = {"Cat","Dog","Pig","Dophin","Mouse","Clown"};
+                alert.setSingleChoiceItems(arrayAnimal, -1, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-
+                        dialogInterface.cancel();
                     }
                 });
-                alert.setPositiveButton("Khong", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
 
-                    }
-                });
-                alert.setNeutralButton("huy", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
 
-                    }
-                });
                 alert.show();
             }
         });
